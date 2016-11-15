0f7853c4bd1aeba879225068356084c86fea5737
KwakesProject <kwakes@me.com>
2016-11-15 16:26:41 +0000

    Add HeroService.getHero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,16 +6,21 @@ import { HEROES } from './mock-heroes'
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+    constructor() { }
 
-  getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
-  }
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
+    }
 
-  getHeroesSlowly(): Promise<Hero[]> {
-    return new Promise<Hero[]>(resolve =>
-      setTimeout(resolve, 2000)) // delay 2 seconds
-      .then(() => this.getHeroes());
-  }
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+    }
+
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(resolve, 2000)) // delay 2 seconds
+            .then(() => this.getHeroes());
+    }
 
 }
