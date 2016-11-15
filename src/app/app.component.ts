ed98b628e045e5ec2548f3fcfad796c914d75f3f
KwakesProject <kwakes@me.com>
2016-11-15 14:50:29 +0000

    Inject the HeroService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,15 @@ import { HeroService } from './hero.service'
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    providers: [HeroService]
 })
 export class AppComponent {
     title = 'Tour of Heroes';
     heroes: Hero[];
     selectedHero: Hero;
+
+ constructor(private heroService: HeroService) { }
 
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
