fb97b90de385a9420d6280ec2095f8d410f6ef50
KwakesProject <kwakes@me.com>
2016-11-18 16:51:26 +0000

    Save hero details

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -30,6 +30,12 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+  save(): void {
+  this.heroService.update(this.hero)
+    .then(() => this.goBack());
+}
+
+
   @Input()
   hero: Hero
 }
